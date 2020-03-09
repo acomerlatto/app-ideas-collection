0f9495f7795e51b0a8561ef50c7e71646bc47f46
Alisson Comerlatto <comerlattoalisson@gmail.com>
2020-03-09 01:44:08 -0300

    Added Bin2Dec

--- Tier-1/bin_2_desc/src/App.vue
+++ Tier-1/bin_2_desc/src/App.vue
@@ -1,28 +1,74 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container mx-auto pt-16">
+    <h1>Bin2Dec</h1>
+    <div class="flex flex-row content-center -mx-2 mt-20">
+      <div class="px-2 ml-auto">
+        <label class="label-form" for="binary">Binary</label>
+        <input
+          id="binary"
+          type="text"
+          class="input-form focus:outline-none focus:bg-white focus:border-gray-500"
+          v-model="binary"
+        />
+        <p
+          v-if="invalidBinary"
+          class="text-red-500 text-xs italic"
+        >Binary invalid! Only 1 and 0 are accepted.</p>
+      </div>
+      <div class="px-2">
+        <label class="label-form" for="decimal">Decimal</label>
+        <input
+          id="decimal"
+          type="text"
+          class="input-form focus:outline-none focus:bg-white focus:border-gray-500"
+          v-model="decimal"
+        />
+      </div>
+
+      <div class="px-2 mr-auto pt-3">
+        <button class="btn btn-blue mt-4" @click="convert()">Convert</button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  name: "App",
+  data() {
+    return {
+      binary: 0,
+      decimal: 0,
+      invalidBinary: false
+    };
+  },
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  watch: {
+    binary(value) {
+      // Verifying if the number inputted is only 0 and 1
+      return (this.invalidBinary = !/^[0-1]+$/.test(value));
+    }
+  },
+
+  methods: {
+    convert() {
+      if (this.invalidBinary) {
+        return;
+      }
+
+      // Reverse Binary to itarete from back
+      const reversedBynary = this.binary
+        .split("")
+        .map(Number)
+        .reverse();
+
+      // Calculate the result applying (value * (base ^ index)) and 
+      // accumulating with the previous result;
+      this.decimal = reversedBynary.reduce(
+        (accumulator, currentValue, index) =>
+          accumulator + currentValue * Math.pow(2, index)
+      );
+    }
+  }
+};
+</script>
